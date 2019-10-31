5d1099d231b249096fe62dae82b1d6510c706e3e
Emilee Mitchell <emcakes@gmail.com>
2019-10-30 20:05:43 -0500

    new setup

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,11 @@
+import firebase from 'firebase';
+import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  // firebase keys that you're loading in. This is the method to initialize firebase
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  // console.log('hi', apiKeys.firebaseKeys);
+};
+
+init();
